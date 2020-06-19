e40e30c768c36cce202a64cb4384811035ef8d65
brandaweppo <65691783+brandaweppo@users.noreply.github.com>
2020-06-19 19:53:50 -0300

    Update envia.py

--- envia.py
+++ envia.py
@@ -9,17 +9,11 @@ class Envia(Bottle):
 		super().__init__()
 		self.route('/', method='POST', callback=self.send)
 		
-#		redis_host = os.getenv('REDIS_HOST', 'fila')
-#		self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
-		#self.fila = redis.StrictRedis(host='fila', port=6379, db=0)
-		
 		db_name = os.getenv('DB_NAME', 'fake')
 		db_user = os.getenv('DB_USER', 'postgres')
 		db_host = os.getenv('DB_HOST', 'db')
 		dsn = f'dbname={db_name} user={db_user} host={db_host}'
 		self.connecta = psycopg2.connect(dsn)
-		#DSN = 'dbname=solicitacoes user=postgres host=db'
-		#self.connecta = psycopg2.connect(DSN)
     
 	def registro_pedido(self, resposta):
 		SQL = 'INSERT INTO pedidos (resposta) VALUES (%s)'
@@ -28,12 +22,10 @@ class Envia(Bottle):
 		self.connecta.commit()
 		cursosql.close()
 		
-		msg = {'resposta': resposta}
-#		self.fila.rpush('envia', json.dumps(msg))   
+		msg = {'resposta': resposta}  
 		
 		print('Mensagem registrada!')
 		
-	#@route('/', method='POST')
 	def send (self):
 		resposta = request.forms.get('resposta')
 		if not resposta:
